e14e7c4119fcb024bc0e9e47414f73d9fd4890d3
Raghav020401 <86782391+Rag-lag@users.noreply.github.com>
2022-01-16 00:56:14 +0300

    Home pg done

---
+++ js/indexjs.js
@@ -0,0 +1,9 @@
+// const hamburger = document.querySelector(".navbar-hamburger");
+// hamburger.addEventListener("click", function() {
+//   const navlinks = document.querySelector(".navbar-links");
+//   if (navlinks.style.visibility === "collapse") {
+//     navlinks.style.visibility = "visible";
+//   } else {
+//     navlinks.style.visibility = "collapse";
+//   }
+// });
