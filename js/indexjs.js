87569f13969316adb649d94c2cc6b494e94bd1d2
Raghav020401 <86782391+Rag-lag@users.noreply.github.com>
2022-01-17 11:46:20 +0300

    Final-1

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,9 +1,33 @@
-// const hamburger = document.querySelector(".navbar-hamburger");
-// hamburger.addEventListener("click", function() {
-//   const navlinks = document.querySelector(".navbar-links");
-//   if (navlinks.style.visibility === "collapse") {
-//     navlinks.style.visibility = "visible";
-//   } else {
-//     navlinks.style.visibility = "collapse";
-//   }
-// });
+var windowlength = window.matchMedia("(max-width:650px)");
+// var reloadlen = window.matchMedia("(min-width:651px)"); //window.innerWidth;
+var hamburger = document.querySelector(".hamburger");
+var navlinks = document.querySelector(".navbar-links");
+var close = document.querySelector(".close");
+
+function collapse() {
+  navlinks.style.visibility = "collapse";
+  close.style.visibility = "collapse";
+  hamburger.style.visibility = "visible";
+  // window.location.reload();
+}
+
+function bigreset() {
+  if (!window.location.hash) {
+    window.location = window.location + '#loaded';
+    window.location.reload();
+  }
+}
+hamburger.addEventListener("click", function() {
+  close.style.visibility = "visible";
+  navlinks.style.visibility = "visible";
+  hamburger.style.visibility = "collapse";
+
+});
+close.addEventListener("click", collapse);
+window.onresize = function() {
+  location.reload();
+}
+// if(roadlen.matches){
+//   bigreset();
+//
+// }
